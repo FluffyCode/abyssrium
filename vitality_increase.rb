102aa7facfdb66eb68ff47e4356a34022e1f6718
FluffyCode <fluffycode@gmail.com>
2017-06-16 17:55:04 -0700

    Touchup output readability with colorize

--- vitality_increase.rb
+++ vitality_increase.rb
@@ -1,12 +1,15 @@
 require 'bigdecimal' # for handling floats
+require 'colorize'   # making output(s) easier to read
 
 def evaluate_all_the_things(array)
   puts "Differences between each Vitality cost and its preceding cost"
 
+  spacer = "  |  ".blue
+
   puts "" # empty space
 
-  puts "             Vitality difference             |  % increase   "
-  puts "--------------------------- + -------------- + --------------" # header row
+  puts "            Vitality difference         " + spacer + "% increase   "
+  puts "-------------------------- + ------------ + --------------".blue # header row
 
   array.each_with_index do |n,i|
     next if i==0 # skip over first value
@@ -16,21 +19,21 @@ def evaluate_all_the_things(array)
     difference = n - array[i-1] # calculate difference
 
     # The difference between each Vitality cost and its preceding cost
-    output_string << "  #{difference}".ljust(25)
+    output_string << "  #{difference}".ljust(25).green
 
-    output_string << "   |   " # row divider
+    output_string << spacer # row divider
 
     # Difference, as above, but limited to 3 decimal places
-    output_string << "#{(BigDecimal.new((difference).to_s).truncate(3).to_f)}".ljust(10)
+    output_string << "#{(BigDecimal.new((difference).to_s).truncate(3).to_f)}".ljust(10).green
 
-    output_string << "   |   " # row divider
+    output_string << spacer # row divider
 
     percent_increase = ((n - array[i-1]) / array[i-1]) * 100 # calculate percentage change
 
     # The percentage increase between each Vitality cost and its preceding cost, limited to 3 decimal places
-    output_string << "#{BigDecimal.new((percent_increase).to_s).truncate(3).to_f}".ljust(6)
+    output_string << "#{BigDecimal.new((percent_increase).to_s).truncate(3).to_f}".ljust(6).green
 
-    output_string << "%"
+    output_string << "%".green
 
     puts output_string
   end
@@ -39,14 +42,14 @@ def evaluate_all_the_things(array)
 
   # Get sum of all Vitality costs
   @sum_of_values = BigDecimal.new((array.inject { |s,i| s+=i }).to_s).truncate(3).to_f
-  puts "Sum of all Vitality costs: #{@sum_of_values}"
+  puts "Sum of all Vitality costs: " + "#{@sum_of_values}".green
 
   puts "" # empty space
 
   # Divide sum by first Vitality cost to get quotient
   # (what you would multiply the initial vitality increase by to get the sum, above)
-  puts "The sum is a #{BigDecimal.new((@sum_of_values / array[0]).to_s).truncate(3).to_f}x
-    increase of the initial amount of #{array[0]}"
+  quotient = "#{BigDecimal.new((@sum_of_values / array[0]).to_s).truncate(3).to_f}x".green
+  puts "The sum is a #{quotient} increase of the initial amount of " + "#{array[0]}".green
 end
 
 # Lonely Corallite
