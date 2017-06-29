16427119909311cce0b63666419ea07d908378ca
FluffyCode <fluffycode@gmail.com>
2017-06-29 08:34:35 -0700

    Add running [Vitality] cost column

--- vitality_increase.rb
+++ vitality_increase.rb
@@ -5,6 +5,7 @@ require 'terminal-table' # for simplifying table creation/output readability
 def evaluate_all_the_things(array,initial_level)
   level = initial_level
   evaluated_rows = [] # initialize empty array to hold TableRow objects
+  running_sum = 0     # initialize running_sum for values
 
   array.each_with_index do |n,i|
     this_row = [] # initialize new TableRow object for the current row
@@ -12,7 +13,8 @@ def evaluate_all_the_things(array,initial_level)
     if i==0 # for the first iteration only...
       this_row << "#{level} -> #{level += 1}".cyan  # ...add initial level increase
       this_row << "#{n}".green                      # ...assign Vitality cost
-      2.times { this_row << "" }                    # ...add 3 empty spaces
+      running_sum += n                              # ...increment running sum
+      3.times { this_row << "" }                    # ...add 3 empty spaces
       evaluated_rows << this_row                    # add this_row to evaluated_rows
       next
     end
@@ -20,6 +22,9 @@ def evaluate_all_the_things(array,initial_level)
     this_row << "#{level} -> #{level += 1}".cyan # add level increase
 
     this_row << "#{n}".green # assign Vitality cost
+
+    # add running sum, limited to 3 decimal places
+    this_row << "#{(BigDecimal.new((running_sum += n).to_s).truncate(3).to_f)}".green
 
     # Calcualte difference, limited to 3 decimal places
     this_row << "#{(BigDecimal.new((n - array[i-1]).to_s).truncate(3).to_f)}".green
@@ -33,7 +38,7 @@ def evaluate_all_the_things(array,initial_level)
   end
 
   # build table from evaluated_rows
-  table = Terminal::Table.new headings: ["level".cyan,"cost".cyan,"difference".cyan,"% increase".cyan],
+  table = Terminal::Table.new headings: ["level".cyan,"cost".cyan,"running cost".cyan,"difference".cyan,"% increase".cyan],
     rows: evaluated_rows
   table.style = { padding_left: 2, padding_right: 2, border_x: "-".blue, border_y: "|".blue, border_i: "+".blue }
   puts table # output table
