40f6c40062d5aabb56c3f2c22ff0b7caaf758d52
FluffyCode <fluffycode@gmail.com>
2017-07-03 11:08:58 -0700

    Unlike previous commit, calculate the actual 30% increase (previous attempt was based on previous value, instead of original)

--- fusion_fish_test.rb
+++ fusion_fish_test.rb
@@ -10,18 +10,20 @@ def calculate_percent_increase(array,type)
   array.each_with_index do |n,i|
     next if i==0 # skip first iteration
 
-    difference = n-array[i-1] # calculate difference
+    # calculate difference
+    difference = n-array[0]
 
-    puts "Original " + type + ": " + "#{array[i-1]}".green
+    puts "Original " + type + ": " + "#{array[0]}".green
     puts "Current "   + type + ": " + "#{n}".green
 
     puts "Difference: " + "#{difference}".green
-    puts "% increase: " + "#{difference/array[i-1]*100}".green # calculate % increase
+    puts "% increase: " + "#{difference/array[0]*100}".green # calculate % increase
 
-    correct_increase = n*0.3 # calculate correct 30% increase
+    # calculate correct 30% increase
+    correct_increase = array[0]*(0.3*i)
 
-    puts "A 30% increase is actually: " + "#{correct_increase}".green
-    puts "Current " + type + " should be: " + "#{array[i-1]+correct_increase}".green
+    puts "A #{30*i}% increase is actually: " + "#{correct_increase}".green
+    puts "Current " + type + " should be: " + "#{array[0]+correct_increase}".green
 
     puts "" #empty space
   end
@@ -29,4 +31,4 @@ end
 
 calculate_percent_increase(x,"Vitality/tap".cyan)
 3.times { puts "" } #empty space
-calculate_percent_increase(y,"Vitality/sec (coral)".cyan)
+# calculate_percent_increase(y,"Vitality/sec (coral)".cyan)
