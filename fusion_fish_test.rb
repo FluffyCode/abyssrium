68db0279d176a7d5d082114240a8f41fd3f21351
FluffyCode <fluffycode@gmail.com>
2017-07-03 10:59:17 -0700

    Calculate correct 30% increase

--- fusion_fish_test.rb
+++ fusion_fish_test.rb
@@ -12,15 +12,21 @@ def calculate_percent_increase(array,type)
 
     difference = n-array[i-1] # calculate difference
 
-    puts "Original " + type.cyan + ": " + "#{array[i-1]}".green
-    puts "Current"   + type.cyan + ": " + "#{n}".green
+    puts "Original " + type + ": " + "#{array[i-1]}".green
+    puts "Current "   + type + ": " + "#{n}".green
 
-    puts "Difference: " + "#{difference}".green 
+    puts "Difference: " + "#{difference}".green
     puts "% increase: " + "#{difference/array[i-1]*100}".green # calculate % increase
+
+    correct_increase = n*0.3 # calculate correct 30% increase
+
+    puts "A 30% increase is actually: " + "#{correct_increase}".green
+    puts "Current " + type + " should be: " + "#{array[i-1]+correct_increase}".green
+
     puts "" #empty space
   end
 end
 
-calculate_percent_increase(x,"Vitality/tap")
+calculate_percent_increase(x,"Vitality/tap".cyan)
 3.times { puts "" } #empty space
-calculate_percent_increase(y,"Vitality/sec (coral)")
+calculate_percent_increase(y,"Vitality/sec (coral)".cyan)
